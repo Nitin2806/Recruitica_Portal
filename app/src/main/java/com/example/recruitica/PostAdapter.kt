d62c4ae8b866476f5353e73333475ca97f1338cf
Nitin Mishra <50167912+Nitin2806@users.noreply.github.com>
2024-03-15 16:42:24 -0400

    Added Details page with post adapters

---
+++ app/src/main/java/com/example/recruitica/PostAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.recruitica
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PostAdapter(private val postList: List<PostData>) : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.post_layout, parent, false)
+        return PostViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val currentItem = postList[position]
+        holder.bind(currentItem)
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
+
+    class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        private val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        private val imageView: ImageView = itemView.findViewById(R.id.imageImageView)
+        private val likesView: TextView = itemView.findViewById(R.id.likesTextView)
+
+        fun bind(post: PostData) {
+
+            val description = post.description ?: ""
+            val title= post.title ?: ""
+            val image= post.imageURL ?: ""
+            val likes = post.likes ?: ""
+            titleTextView.text= title
+            likesView.text= likes.toString()
+            contentTextView.text = description
+        }
+    }
+}
